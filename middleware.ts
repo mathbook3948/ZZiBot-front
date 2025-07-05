3bbacc42ee174306037b9df7afca069cbb68fe7e
mathbook3948 <yuno.jung.07@gmail.com>
2025-07-05 23:49:39 +0900

    modify: middleware 검증 수정

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { jwtVerify } from 'jose'
+import {NextRequest, NextResponse} from 'next/server'
+import {jwtVerify} from 'jose'
 
 const PUBLIC_PATHS = ['/login', '/']
 
@@ -10,32 +10,53 @@ export async function middleware(request: NextRequest) {
     const isStatic = pathname.match(/\.\w+$/) || pathname.startsWith('/_next/image')
     const isApi = pathname.startsWith('/api')
     const isAdminLogin = pathname.startsWith('/admin/login')
-    const isPublic =  PUBLIC_PATHS.includes(pathname)
+    const isPublic = PUBLIC_PATHS.includes(pathname)
     if (isStatic || isPublic || isApi || isAdminLogin) {
         return NextResponse.next()
     }
 
-    // 토큰 쿠키 읽기
+    // 권한 처리===================================================================
     const token =
         request.cookies.get('__Secure-authjs.session-token')?.value ??
         request.cookies.get('next-auth.session-token')?.value
 
     if (!token) {
-        return NextResponse.redirect(new URL('/login', request.url))
+        if (pathname.startsWith('/admin')) {
+            return NextResponse.redirect(new URL('/admin/login', request.url))
+        } else {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
     }
 
     try {
         // JWT 검증
         const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET)
-        const { payload } = await jwtVerify(token, secret)
+        const {payload} = await jwtVerify(token, secret)
+        const roles = payload.roles as string[]
 
-        // 권한 검사 (예시)
-        if (pathname.startsWith('/admin') && payload.role !== 'ADMIN') {
-            return NextResponse.redirect(new URL('/403', request.url))
+        if (pathname.startsWith('/admin')) {
+            //관리자
+            if (!token) {
+                return NextResponse.redirect(new URL('/admin/login', request.url))
+            }
+
+            if (!(roles.includes("ADMIN") || roles.includes("SUPERADMIN"))) {
+                return NextResponse.redirect(new URL('/', request.url))
+            }
+        } else {
+            //사용자
+            if (!token) {
+                return NextResponse.redirect(new URL('/login', request.url))
+            }
         }
 
         return NextResponse.next()
-    } catch (err) {
-        return NextResponse.redirect(new URL('/login', request.url))
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (_) {
+        if (pathname.startsWith('/admin')) {
+            return NextResponse.redirect(new URL('/admin/login', request.url))
+        } else {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
     }
 }
